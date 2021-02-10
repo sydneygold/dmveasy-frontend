15f9e26da27c029cefe2d8e1a8e904bbc9655f04
Music <musicm987@gmail.com>
2021-02-10 13:49:55 -0700

    start potential get started form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './assets/output.css'
 import MainPage from './components/MainPage'
+// import GetStarted from './components/GetStarted'
 
 function App() {
   return (
     <div className="App">
       <MainPage />
+      {/* <GetStarted /> */}
     </div>
   );
 }
