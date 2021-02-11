50c5ed4eda7dc23509ce6f5b2634ba30b787c342
Tiffany Kanjanabout <tiffanynitk@gmail.com>
2021-02-10 17:34:20 -0700

    started login form styling

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,22 +3,29 @@ import logo from '../images/colorado-logo.png';
 
 export default function Login() {
     return (
-        <div className='main-login'>
-            <div className='logo-login'>
+        <div className='container max-w-md mx-auto md:max-w-2xl'>
+            <div className='flex justify-center logo-login'>
                 <img 
+                    className="h-24"
                     src={logo}
                 />
             </div>
-            <form id='login-form'>
+            <form 
+                className='flex flex-col items-center shadow-md' 
+                id='login-form'>
+                <h1>Login</h1>
                 <input 
+                    className='w-1/2 m-5 border-b'
                     type='text'
                     placeholder='email'
                 />
                 <input 
+                    className='w-1/2 m-5 border-b'
                     type='password'
                     placeholder='password'
                 />
                 <input 
+                    className='rounded-full py-3 px-6 m-5'
                     type='submit'
                     value='submit'
                 />
