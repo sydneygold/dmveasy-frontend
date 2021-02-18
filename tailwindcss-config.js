b466e9b4a0a9f6428dca03df9b875dca0c0e4a14
Music <musicm987@gmail.com>
2021-02-17 22:03:33 -0700

    this one is better I accidentally messed up other tailwind file

--- tailwindcss-config.js
+++ tailwindcss-config.js
@@ -6,10 +6,6 @@ module.exports = {
       fontFamily: {
         "Josefin Slab": ["Josefin Slab", 'sans']
       },
-      backgroundImage: theme => ({
-        'hero-pattern': "url('/img/hero-pattern.svg')",
-        'footer-texture': "url('/img/footer-texture.png')",
-      })
     },
   },
   variants: {
